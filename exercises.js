198ffd34a89a41fa5065f2faa114ab4d4d4276e1
Chris M <robotpuppetmaster@gmail.com>
2017-03-17 17:06:46 -0400

    Completed extra credit problem

--- exercises.js
+++ exercises.js
@@ -125,6 +125,16 @@ function cacheFunction(cb) {
 	//if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
 	//if the function you return is invoked again with 5 it will look on an object in the closure scope
 	//and return 25 directly and will not invoke cb again
+  var cache = {    };
+
+  function squaredCache(num){
+    if(!cache.hasOwnProperty(num)){
+      cache[num] = cb(num);
+    }
+    return cache[num];
+  }
+
+  return squaredCache;
 }
 
 
